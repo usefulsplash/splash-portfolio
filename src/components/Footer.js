e1d862ea7a132ea4866a370eb35d14d63f3416ba
usefulsplash <emjavier16@gmail.com>
2022-09-26 18:03:28 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import "../styles/Footer.css"
+import React from "react";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div id='footer'>
-        <div className='credits'>Built and designed by Enrique Javier.</div>
-        <div className='credits'>All rights reserved. &copy;</div>
+    <div id="footer">
+      <div className="credits">Built and designed by Enrique Javier.</div>
+      <div className="credits">All rights reserved. &copy; 2022</div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
